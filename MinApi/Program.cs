de568374d9a063b1f6a052696ab0fe3fc1d3c75f
Jason De Lorme <jasondel@google.com>
2021-06-07 17:44:37 -0400

    Minimal API sample.

--- MinApi/Program.cs
+++ MinApi/Program.cs
@@ -1,31 +1,11 @@
 using System;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using jasondel;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SchoolContext>(options => 
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("SchoolContext")));
 await using var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
+app.MapGet("/", (Func<string>)(() => "Hello World!"));
 app.MapGet("/{name?}", (Func<string, string>)PrintHello);
-
-app.MapGet("/person/{name}", 
-    (Func<string, object>)((name) => new { Person = name }));
-
-app.MapGet("/animal", (Func<string>)(() => "meow"));
-
-app.MapGet("/students", (Func<IEnumerable<Student>>)GetStudents);
 
 await app.RunAsync();
 
@@ -35,12 +15,4 @@ string PrintHello(string name)
         return "You're Brian???";
     else
         return $"Hello {name}!";
-}
-
-IEnumerable<Student> GetStudents()
-{
-    var scope = app.Services.CreateScope();
-    var school = scope.ServiceProvider.GetService<SchoolContext>();
-    
-    return school.Students;
 }
