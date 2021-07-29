35d38fc3f3d9cfd28f6caa6439f02f2a3b500f23
Jason De Lorme <jasondel@google.com>
2021-07-29 13:51:29 +0000

    Clean up for demo.

--- MinApi/Program.cs
+++ MinApi/Program.cs
@@ -5,15 +5,6 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
-app.MapGet("/{name?}", (Func<string, string>)PrintHello);
+app.MapGet("/", () => "Hello Masterclass!");
 
 await app.RunAsync();
-
-string PrintHello(string name)
-{
-    if (name?.ToLower() == "brian")
-        return "You're Brian???";
-    else
-        return $"Hello {name}!";
-}
